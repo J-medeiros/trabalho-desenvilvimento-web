b9580ce3ce387a2291b55a5467ddbc314495456e
J-medeiros <joao.v.m.s.jacinto@academico.unirv.edu.br>
2025-05-08 19:40:05 -0300

    Ajustando repositorio

--- desenvolvimento-wev/src/services/UserService.tsx
+++
@@ -1,9 +0,0 @@
-const users: string[] = ["Joao Vitor", "Joao Paulo", "Beatriz", ];
-
-export function getUsers() {
-  return users;
-}
-
-export function addUser(name: string) {
-  users.push(name);
-}
